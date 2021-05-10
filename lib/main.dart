2a196495d257b03b968c10d8dffa8726f3e3df09
franciscrispin <franciscrispingoh@gmail.com>
2021-05-10 09:19:20 +0800

    Add layout examples

--- lib/main.dart
+++ lib/main.dart
@@ -5,75 +5,68 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Counter(),
+          child: FlexExample(),
         ),
       ),
     ),
   );
 }
 
-class Counter extends StatefulWidget {
-  @override
-  // When createState is called, a State object is created and is associated
-  // with a BuildContext.
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  @override
-  // The first method called when the widget is created.
-  // initState is called once and only once.
-  void initState() {
-    super.initState();
-    print('counter initState');
-  }
-
-  void _increment() {
-    // Notifies the widget about state changes which causes the widget to
-    // rebuild.
-    setState(() {
-      ++_counter;
-    });
-  }
-
+class FlexExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('counter build');
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+      color: Colors.grey,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text('Hello'),
+          BlueBox(),
+          BlueBox(),
+          BlueBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 75,
+      height: 75,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+class StackExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        CounterIncrementor(onPressed: _increment),
-        SizedBox(width: 16),
-        CounterDisplay(count: _counter),
+        // Bottom-most widget
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
+        Container(
+          width: 90,
+          height: 90,
+          color: Colors.green,
+        ),
+        // Top-most widget
+        Container(
+          width: 30,
+          height: 30,
+          color: Colors.blue,
+        ),
       ],
     );
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  // In a stateless widget, the build function only runs once.
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
-    );
-  }
-}
-
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({required this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
   }
 }
